aa1152dd2b67e0f95b732a336d40562823251021
Josh Cheek <josh.cheek@gmail.com>
2018-02-02 10:07:07 -0600

    Store variables as a thread/fiber local var
    So we don't have to pass them around all the time.

--- lib/joshua_script.rb
+++ lib/joshua_script.rb
@@ -2,8 +2,6 @@ require 'fiber'
 require 'joshua_script/ast'
 require 'joshua_script/parser'
 
-# TODO:
-# * make vars fiber local so we don't have to pass them through every call to evaluate
 
 class JoshuaScript
   ESPARSE = File.expand_path "../node_modules/.bin/esparse", __dir__
@@ -43,7 +41,10 @@ class JoshuaScript
       if work.respond_to? :resume
         work.resume
       else
-        Fiber.new { result = evaluate work, [@globals] }.resume
+        Fiber.new do
+          Thread.current[:scopes] = [@globals]
+          result = evaluate work
+        end.resume
       end
     end
     result
@@ -54,22 +55,22 @@ class JoshuaScript
   # options are passed b/c of the output of esprima.
   # If this was for real, I'd transform their AST into my own internal
   # representation, but this is really just a thought experiment, so whatevz.
-  def evaluate(ast, vars, identifier: :resolve)
+  def evaluate(ast, identifier: :resolve)
     return ast unless ast
 
     # it's an internal method: a block, method, or continuation
     if ast.respond_to? :call
-      return invoke(ast, vars, [], ast: ast)
+      return invoke(ast, [], ast: ast)
     end
 
     case ast[:type]
     when 'Program'
-      ast[:body].map { |child| evaluate child, vars }.last
+      ast[:body].map { |child| evaluate child }.last
 
     when 'Identifier'
       name = ast[:name]
       if identifier == :resolve
-        scope = find_scope vars, name
+        scope = find_scope scopes, name
         raise "Undefined: #{name}" if !scope # FIXME: untested temp(?) hack
         scope[name]
       else
@@ -77,15 +78,15 @@ class JoshuaScript
       end
 
     when 'ExpressionStatement'
-      evaluate ast[:expression], vars
+      evaluate ast[:expression]
 
     when 'Invooooooooke!' # FIXME: this is an internal call, unify it w/ the others
-      invoke ast[:invokable], ast[:vars], ast[:args], ast: ast
+      invoke ast[:invokable], ast[:args], ast: ast
 
     when 'CallExpression'
-      method = evaluate ast[:callee], vars
-      args   = ast[:arguments].map { |arg| evaluate arg, vars }
-      invoke method, vars, args, ast: ast
+      method = evaluate ast[:callee]
+      args   = ast[:arguments].map { |arg| evaluate arg }
+      invoke method, args, ast: ast
 
     when 'Literal'
       value = ast.fetch :value
@@ -93,47 +94,51 @@ class JoshuaScript
       value
 
     when 'BlockStatement'
-      ast[:body].map { |child| evaluate child, vars }.last
+      ast[:body].map { |child|
+        # FIXME: `let` will need this to push/pop new vars, I think
+        evaluate child
+      }.last
 
     when 'ArrayExpression'
-      ast.fetch(:elements).map { |child| evaluate child, vars }
+      ast.fetch(:elements).map { |child| evaluate child }
 
     when 'ObjectExpression'
       ast[:properties].each_with_object({}) do |prop, obj|
-        key   = evaluate prop[:key],   vars, identifier: :to_s
-        value = evaluate prop[:value], vars
+        key   = evaluate prop[:key], identifier: :to_s
+        value = evaluate prop[:value]
         obj[key] = value
       end
 
     when 'BinaryExpression'
       operator = ast[:operator]
-      left     = evaluate ast[:left], vars
-      right    = evaluate ast[:right], vars
+      left     = evaluate ast[:left]
+      right    = evaluate ast[:right]
       left.send operator, right
 
     when 'VariableDeclaration'
-      ast[:declarations].each { |dec| evaluate dec, vars }
+      ast[:declarations].each { |dec| evaluate dec }
 
     when 'VariableDeclarator'
       name  = ast[:id][:name]
-      value = evaluate ast[:init], vars
-      vars.last[name] = value
+      value = evaluate ast[:init]
+      scopes.last[name] = value
 
     when 'AssignmentExpression'
-      name  = evaluate ast.fetch(:left), vars, identifier: :to_string
-      value = evaluate ast.fetch(:right), vars
-      scope = find_scope vars, name
+      name  = evaluate ast.fetch(:left), identifier: :to_string
+      value = evaluate ast.fetch(:right)
+      scope = find_scope scopes, name
       scope[name] = value
 
     when 'ArrowFunctionExpression', 'FunctionExpression'
-      ast[:scope] = vars.dup # make it a closure
+      # FIXME: set this on the fn
+      ast[:scopes] = scopes.dup # make it a closure
       ast
 
     when 'FunctionDeclaration'
-      ast[:scope] = vars.dup # this is why it's a closure
+      ast[:scopes] = scopes.dup # this is why it's a closure
       name = ast[:id]
-      name = evaluate name, vars, identifier: :to_s if name
-      vars.last[name] = ast
+      name = evaluate name, identifier: :to_s if name
+      scopes.last[name] = ast
       ast
 
     when 'EmptyStatement' # I think it's from a semicolon on its own line
@@ -142,12 +147,12 @@ class JoshuaScript
     when 'ReturnStatement'
       # FIXME: need a way to bail on the fn if we want to return early
       # right now it only works b/c the return statements are the last line
-      evaluate ast[:argument], vars
+      result = evaluate ast[:argument]
 
     when 'MemberExpression'
-      object  = evaluate ast[:object], vars
+      object  = evaluate ast[:object]
       id_type = ast[:computed] ? :resolve : :to_s
-      prop    = evaluate ast[:property], vars, identifier: id_type
+      prop    = evaluate ast[:property], identifier: id_type
       # Hacky bs that allows us to get some useful functionality now,
       # without implementing prototypes or `this`
       begin
@@ -162,7 +167,7 @@ class JoshuaScript
             when :forEach
               lambda do |cb, **|
                 object.each do |e|
-                  invoke cb, vars, [e], ast: ast
+                  invoke cb, [e], ast: ast
                 end
               end
             end
@@ -173,27 +178,27 @@ class JoshuaScript
       end
 
     when 'IfStatement', 'ConditionalExpression'
-      test = evaluate ast[:test], vars
+      test = evaluate ast[:test]
       if test
-        evaluate ast[:consequent], vars
+        evaluate ast[:consequent]
       else
-        evaluate ast[:alternate], vars
+        evaluate ast[:alternate]
       end
 
     when 'UpdateExpression'
-      name        = evaluate ast[:argument], vars, identifier: :to_s
-      scope       = find_scope vars, name
+      name        = evaluate ast[:argument], identifier: :to_s
+      scope       = find_scope scopes, name
       pre_value   = scope[name]
       method      = ast[:operator][0] # plus or minus
       scope[name] = scope[name].public_send(method, 1)
       ast[:prefix] ? scope[name] : pre_value
 
     when 'ForStatement'
-      evaluate ast[:init], vars
+      evaluate ast[:init]
       loop do
-        break unless evaluate ast[:test], vars
-        evaluate ast[:body], vars
-        evaluate ast[:update], vars
+        break unless evaluate ast[:test]
+        evaluate ast[:body]
+        evaluate ast[:update]
       end
 
     else
@@ -208,24 +213,35 @@ class JoshuaScript
     raise NotImplementedError, 'lol not even implemented', caller
   end
 
-  private def find_scope(vars, name)
-    vars.reverse_each.find { |scope| scope.key? name }
+  private def scopes
+    Thread.current[:scopes]
+  end
+
+  private def find_scope(scopes, name)
+    scopes.reverse_each.find { |scope| scope.key? name }
   end
 
   private def get_line(ast)
     ast.fetch(:loc).fetch(:end).fetch(:line)
   end
 
-  private def invoke(invokable, vars, args, ast:)
+  private def invoke(invokable, args, ast:)
     if invokable.respond_to? :call
       invokable.call *args, ast: ast
     else
-      evaluate invokable[:body], [
-        *invokable[:scope],
+      old_scopes = scopes()
+      Thread.current[:scopes] = [
+        *invokable[:scopes],
         invokable[:params]
-          .map { |param| evaluate param, vars, identifier: :to_string }
+          .map { |param| evaluate param, identifier: :to_string }
           .zip(args)
-          .to_h]
+          .to_h
+      ]
+      begin
+        evaluate invokable[:body]
+      ensure
+        Thread.current[:scopes] = old_scopes
+      end
     end
   end
 
@@ -237,7 +253,7 @@ class JoshuaScript
     cb &&= {
       type:      'Invooooooooke!',
       invokable: cb,
-      vars:      [@globals],
+      scope:     [@globals],
       args:      [],
       loc:       ast.loc,
     }
